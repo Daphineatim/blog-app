ad9d85db4ba8d7bfeb46805f8b6629faa78f7e50
Daphineatim <hopedaphine19@gmail.com>
2023-02-01 06:49:59 -0700

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     end
   
     # PATCH/PUT /users/1 or /users/1.json
-    def update
+  def update
       respond_to do |format|
         if @user.update(user_params)
           format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
@@ -41,30 +41,29 @@ class UsersController < ApplicationController
         else
           format.html { render :edit, status: :unprocessable_entity }
           format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
-      end
     end
-  
+  end
+end
+
     # DELETE /users/1 or /users/1.json
-    def destroy
+  def destroy
       @user.destroy
-  
+
       respond_to do |format|
         format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
         format.json { head :no_content }
-      end
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-  
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:Name, :Photo, :Bio, :PostsCounter)
-    end
   end
-  
+end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+  def set_user
+      @user = User.find(params[:id])
+  end
+
+    # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:Name, :Photo, :Bio, :PostsCounter)
+  end
+end
