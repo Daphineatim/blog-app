df6022ce35e5273be5c776f8a6b7194801b7bd91
Daphineatim <hopedaphine19@gmail.com>
2023-02-02 11:01:07 -0700

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
   def last_5_comments
     Comment.where(post_id: id).order(created_at: :desc).limit(5)
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
 
   def update_post_count_for_user
     user = User.find_by(id: user_id)
-    counter = Post.where(user_id: user.id).count
+    count = Post.where(user_id: user.id).count
     author.increment!(:posts_counter)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, class_name: 'post', foreign_key: 'author_id'
+  has_many :likes, class_name: 'likes', foreign_key: 'author_id'
+  has_many :comments, class_name: 'comments', foreign_key: 'author_id'
 
   def last_3_posts
     Post.order(created_at: :desc).limit(3)
