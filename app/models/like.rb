c91d50c483c26646ae1f28be1b86d995e07b0ebe
Daphineatim <hopedaphine19@gmail.com>
2023-02-02 09:33:13 -0700

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  
+
   def update_like_counter_for_post
     post = Post.find_by(id: post_id)
     count = Like.where(post_id: post.id).count
