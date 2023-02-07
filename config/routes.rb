7062debbc217875a197b93b7f20ff510b1320d49
Daphineatim <hopedaphine19@gmail.com>
2023-02-07 07:21:07 -0700

    Update user index and show html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+  resources :posts, only: [:index, :show]
   resources :comments
-  resources :posts
   resources :likes
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
