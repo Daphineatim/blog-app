6ee5b539bdd2a60ffb99cf6726f78492ff7db355
Daphineatim <hopedaphine19@gmail.com>
2023-02-07 07:09:01 -0700

    Create show.html.erb and index.html.erb for posts and style it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: 'post', foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
   has_many :likes, class_name: 'likes', foreign_key: 'author_id'
-  has_many :comments, class_name: 'comments', foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 
   validates :name, presence: true, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
