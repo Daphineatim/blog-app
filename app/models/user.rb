ac12b8996b0f8ca0fdead618530bfd569e17718c
Daphineatim <hopedaphine19@gmail.com>
2023-02-17 04:57:58 -0700

    Update devise folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+
+  after_initialize :set_defaults
 
   validates :name, presence: true, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_3_posts
     Post.where(author_id: id).order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_defaults
+    self.posts_counter ||= 0
   end
 end
